60079fdd69d8f15ab4627e6cee1cd046f83cde9b
Shaquille Williams <swilliams9772@gmail.com>
2024-11-23 14:09:13 -0500

    Setup GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Enable static exports
-  basePath: '',      // Base path for GitHub Pages
+  output: 'export',
+  basePath: '',
   images: {
     unoptimized: true,
   },
